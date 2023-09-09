b2d17ed32ef1230ebb86e550b076b87a66f927f2
Şerife Tezel <serife.tezel@vomsis.com>
2023-09-10 00:13:34 +0300

    Working alert dialog

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var password2Text: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpClicked(_ sender: Any) {
+        /*
+        let alert = UIAlertController(title: "Error", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            //button clicked
+            print("button clicked")}
+        alert.addAction(okButton)
+        self.present(alert,animated: true, completion: nil)
+        */
+        
+        if usernameText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Username not found")
+            
+        }
+        else if passwordText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Password not found")
+            
+        } else if passwordText.text != password2Text.text{
+            makeAlert(titleInput: "Error", messageInput: "Password not match")
+           
+        } else {
+            makeAlert(titleInput: "Success", messageInput: "User OK")
+        }
+    }
+     
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title:  titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
